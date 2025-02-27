8d25393f96e655131eb9b535877a518982509975
Linhtranne <tuhithanahihi@gmail.com>
2025-02-27 16:57:40 +0700

    hehe

---
+++ ss1/src/Ex2.java
@@ -0,0 +1,6 @@
+public class Ex2 {
+    public static void main(String[] args) {
+        System.out.printf("Tôi tên là %s, tôi %d tuổi, hiện tại đang học ở PTIT_HN, Quê ở %s","Trần Quang Linh",20,"Tuyên Quang");
+    }
+}
+
